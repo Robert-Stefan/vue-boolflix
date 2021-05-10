9bc18050acb6697a073a29119b5f1b2c0a53ca12
Robert-Stefan <robertboss05@hotmail.it>
2021-05-10 14:15:50 +0200

    Add files and add Header

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="header">
+        <h1>BOOLFLIX</h1>
+        <div class="search">
+            <input
+                type="search"
+                id="site-search"
+                name="netflix"
+                aria-label="Search through site content"
+            />
+
+            <button>Search</button>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Header',
+};
+</script>
+
+<style scoped lang="scss">
+.header {
+    display: flex;
+    justify-content: space-between;
+    h1,
+    .search {
+        padding-top: 20px;
+        input {
+            outline: none;
+        }
+    }
+}
+</style>
